c7e1f45ef160326ff193ea0af05ccfc75483960a
shwetadevkar006-eng <shwetadevkar006@gmail.com>
2026-09-11 12:46:55 +0530

    Add files via upload

--- practical9.py
+++ practical9.py
@@ -1,1 +1,27 @@
-   
+   #Student Marks Management System
+print("------------------------- Student Marks Management System ---------------------------")
+
+marks = [70, 80, 65, 90]
+
+# Display marks - Traversal
+print("Original Marks:")
+for mark in marks:
+    print(mark)
+
+# Insert a new mark
+marks.insert(2, 75)
+
+print("\nAfter Insertion:")
+print(marks)
+
+# Update a mark
+marks[1] = 85
+
+print("\nAfter Updating:")
+print(marks)
+
+# Delete a mark
+marks.remove(65)
+
+print("\nAfter Deletion:")
+print(marks)
